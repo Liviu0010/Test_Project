50ec3787d8c99b9db0a2a60b856b34edbeabefa7
Liviu0010 <Liviu0010@gmail.com>
2015-04-22 10:39:57 +0300

    t

--- queue.hpp
+++ queue.hpp
@@ -1,6 +1,14 @@
 #ifndef QUEUE_HPP_INCLUDED
 #define QUEUE_HPP_INCLUDED
 
-
+template <class Tip>
+class Queue
+{
+    struct Node
+    {
+        Tip val;
+        Node *next;
+    }start;
+};
 
 #endif // QUEUE_HPP_INCLUDED
