017409032b309bef1794cf5a1d8b9db9e5f96ff2
Liviu0010 <Liviu0010@gmail.com>
2015-04-22 11:12:25 +0300

    queue ready

--- queue.hpp
+++ queue.hpp
@@ -7,8 +7,69 @@ class Queue
     struct Node
     {
         Tip val;
-        Node *next;
-    }start;
+        Node *pre;
+
+        Node()
+        {
+            pre = NULL;
+        }
+
+        Node(Tip toInsert)
+        {
+            val = toInsert;
+            pre = NULL;
+        }
+    };
+
+    Node *beg, *end;
+
+public:
+    Queue()
+    {
+        beg = end = NULL;
+    }
+
+    Tip front()
+    {
+        if(beg != NULL)
+            return beg->val;
+    }
+
+    bool empty()
+    {
+        return (beg==NULL);
+    }
+
+    void pop()
+    {
+        Node *p = beg;
+
+        if(beg == NULL)
+            return;
+
+        if(beg == end)
+            end = NULL;
+
+        beg = beg->pre;
+
+        delete p;
+    }
+
+    void push(Tip toInsert)
+    {
+        if(beg == NULL)
+            beg = new Node(toInsert);
+            else if(beg != NULL && end == NULL)
+            {
+                end = new Node(toInsert);
+                beg->pre = end;
+            }
+                else
+                {
+                    end->pre = new Node(toInsert);
+                    end = end->pre;
+                }
+    }
 };
 
 #endif // QUEUE_HPP_INCLUDED
